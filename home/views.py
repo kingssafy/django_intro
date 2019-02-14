b6efed4dca7140132a8ec354a860a23664debc69
kingssafy <onlyforfile@gmail.com>
2019-02-14 14:36:59 +0900

    11_Template inheritance_2

--- home/views.py
+++ home/views.py
@@ -7,36 +7,36 @@ def index(request):
     print(request)
     print(type(request))
     pprint(request.META)
-    return render(request, 'index.html')
+    return render(request, 'home/index.html')
 
 def dinner(request):
     menues = ["tuna", "pastas", "bob", "jjjige", "sushi"]
-    return render(request, 'dinner.html', {'menues': menues})
+    return render(request, 'home/dinner.html', {'menues': menues})
 
 def hello(request, name):
     context = {"name": name}
-    return render(request, 'hello.html', context)
+    return render(request, 'home/hello.html', context)
 
 def cube(request, num):
     context = {"num": int(num)**3}
-    return render(request, "cube.html", context)
+    return render(request, "home/cube.html", context)
 
 def ping(request):
-    return render(request, "ping.html")
+    return render(request, "home/ping.html")
 
 def pong(request):
     print(request.GET)
     data = request.GET.get('data')
-    return render(request, "pong.html", {"data": data})
+    return render(request, "home/pong.html", {"data": data})
 
 def user_new(request):
     
-    return render(request, "user_new.html")
+    return render(request, "home/user_new.html")
 
 def user_create(request):
     nickname = request.POST.get('nickname')
     pwd = request.POST.get('pwd')
-    return render(request, 'user_create.html', {'nickname': nickname, 'pwd': pwd})
+    return render(request, 'home/user_create.html', {'nickname': nickname, 'pwd': pwd})
 
 def template_example(request):
     my_list = ['jjajang', 'tansu', 'jjambbong', 'yangjang']
@@ -44,7 +44,7 @@ def template_example(request):
     messages = ["apple", "banana", "cucumber", "mango"]
     empty_list = []
     datetimenow = datetime.now()
-    return render(request, 'template_example.html',
+    return render(request, 'home/template_example.html',
             {'my_list': my_list,
              'my_sentence': my_sentence,
              'messages': messages, 'empty_list': empty_list,
@@ -53,4 +53,4 @@ def template_example(request):
             )
 
 def static_example(request):
-    return render(request, 'static_example.html')
+    return render(request, 'home/static_example.html')
