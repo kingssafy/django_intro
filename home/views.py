5fd66b365ac13a5fcc7abb613106124e64eea83f
kingssafy <onlyforfile@gmail.com>
2019-02-14 10:21:59 +0900

    07_DTL_Practive

--- home/views.py
+++ home/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, HttpResponse
 from pprint import pprint
 import random
+from datetime import datetime
 # Create your views here.
 def index(request):
     print(request)
     print(type(request))
     pprint(request.META)
-    return HttpResponse("Welcome to Djang!")
+    return render(request, 'index.html')
 
 def dinner(request):
     menues = ["tuna", "pastas", "bob", "jjjige", "sushi"]
@@ -36,3 +37,18 @@ def user_create(request):
     nickname = request.POST.get('nickname')
     pwd = request.POST.get('pwd')
     return render(request, 'user_create.html', {'nickname': nickname, 'pwd': pwd})
+
+def template_example(request):
+    my_list = ['jjajang', 'tansu', 'jjambbong', 'yangjang']
+    my_sentence = 'Life is short, you need python'
+    messages = ["apple", "banana", "cucumber", "mango"]
+    empty_list = []
+    datetimenow = datetime.now()
+    return render(request, 'template_example.html',
+            {'my_list': my_list,
+             'my_sentence': my_sentence,
+             'messages': messages, 'empty_list': empty_list,
+             'datetimenow': datetimenow,
+             }
+            )
+
