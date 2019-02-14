aea9398e3f7dd5c296e3b3c3c8c722179912572e
kingssafy <onlyforfile@gmail.com>
2019-02-14 11:32:29 +0900

    09_Separate URL for multiple apps

--- django_intro/settings.py
+++ django_intro/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = ['django-intro-kingssafy.c9users.io', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'utilities.apps.UtilitiesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     # my apps
     'home.apps.HomeConfig',
 ]
